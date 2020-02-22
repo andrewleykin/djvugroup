28b51342335586a6409e7754f3d02ad32b5987e3
Kistanov <rizzydizzy@mail.ru>
2020-02-22 18:34:40 +0300

    финалка, без правок

--- build/app/js/app.js
+++ build/app/js/app.js
@@ -1,8 +1,23 @@
 // Начальная функция
+$('.hero__pag-item').first().addClass('active');
+$('.hero__pag-item').on('click', function(){
+	$('.hero__pag-item').each(function(){
+		$(this).removeClass('active')
+	});
+	$(this).addClass('active');
+	$('.hero__pag-item').each(function(index){
+		if($(this).hasClass('active')) {
+			$('.hero__slider').css('left', -100 * index + "%")
+		}
+	})
+})
 
-(function(){
-	console.log('app');
-})();
+
 $(document).ready(function () {
     svg4everybody({});
 });
+// Библиотека wow.js для анимации
+
+(function () {
+	new WOW().init();
+})();
--- dev/static/js/app.js
+++ dev/static/js/app.js
@@ -1,5 +1,14 @@
 // Начальная функция
+$('.hero__pag-item').first().addClass('active');
+$('.hero__pag-item').on('click', function(){
+	$('.hero__pag-item').each(function(){
+		$(this).removeClass('active')
+	});
+	$(this).addClass('active');
+	$('.hero__pag-item').each(function(index){
+		if($(this).hasClass('active')) {
+			$('.hero__slider').css('left', -100 * index + "%")
+		}
+	})
+})
 
-(function(){
-	console.log('app');
-})();
