4d5b47469b51c02535a421d2abfea698e6d89c24
Kistanov <rizzydizzy@mail.ru>
2020-02-22 19:21:07 +0300

    Без слайдера

--- build/app/js/app.js
+++ build/app/js/app.js
@@ -1,16 +1,19 @@
-// Начальная функция
-$('.hero__pag-item').first().addClass('active');
-$('.hero__pag-item').on('click', function(){
-	$('.hero__pag-item').each(function(){
-		$(this).removeClass('active')
-	});
-	$(this).addClass('active');
-	$('.hero__pag-item').each(function(index){
-		if($(this).hasClass('active')) {
-			$('.hero__slider').css('left', -100 * index + "%")
-		}
-	})
-})
+// // Начальная функция
+// $('.hero__slider-item').each(function(){
+// 	$('.hero__pag').append('<li class="hero__pag-item"></li>')
+// })
+// $('.hero__pag-item').first().addClass('active');
+// $('.hero__pag-item').on('click', function(){
+// 	$('.hero__pag-item').each(function(){
+// 		$(this).removeClass('active')
+// 	});
+// 	$(this).addClass('active');
+// 	$('.hero__pag-item').each(function(index){
+// 		if($(this).hasClass('active')) {
+// 			$('.hero__slider').css('left', -100 * index + "%")
+// 		}
+// 	})
+// })
 
 
 $(document).ready(function () {
