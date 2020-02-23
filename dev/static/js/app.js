aac068c875afcd23146401b0b42b061fc4c7e8c5
Kistanov <rizzydizzy@mail.ru>
2020-02-23 13:21:18 +0300

    Засунул все в ссылки, иконки не трогал

--- dev/static/js/app.js
+++ dev/static/js/app.js
@@ -1,17 +1,1 @@
-// // Начальная функция
-// $('.hero__slider-item').each(function(){
-// 	$('.hero__pag').append('<li class="hero__pag-item"></li>')
-// })
-// $('.hero__pag-item').first().addClass('active');
-// $('.hero__pag-item').on('click', function(){
-// 	$('.hero__pag-item').each(function(){
-// 		$(this).removeClass('active')
-// 	});
-// 	$(this).addClass('active');
-// 	$('.hero__pag-item').each(function(index){
-// 		if($(this).hasClass('active')) {
-// 			$('.hero__slider').css('left', -100 * index + "%")
-// 		}
-// 	})
-// })
 
