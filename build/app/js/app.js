5f571ac65a46d49ca995a78e7f789bba68b85163
Andrew Leykin <katashi1328@mail.ru>
2020-02-25 07:58:49 +0300

    add slider

--- build/app/js/app.js
+++ build/app/js/app.js
@@ -1,4 +1,13 @@
-
+(function() {
+  $('.hero__slider').slick({
+    arrows: false,
+    dots: true,
+    dotsClass: 'hero__pag',
+    customPaging: function() {
+      return '<span"></span>'
+    }
+  })
+})()
 
 $(document).ready(function () {
     svg4everybody({});
--- dev/static/js/app.js
+++ dev/static/js/app.js
@@ -1,1 +1,10 @@
-
+(function() {
+  $('.hero__slider').slick({
+    arrows: false,
+    dots: true,
+    dotsClass: 'hero__pag',
+    customPaging: function() {
+      return '<span"></span>'
+    }
+  })
+})()
